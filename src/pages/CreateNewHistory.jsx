7bf9894b42bdddde918996062fb8fd4a2c91ce7a
TepChoulong <tepchoulongzin@gmail.com>
2024-02-25 19:23:01 +0700

    Completed design the createNewHistory-page

--- src/pages/CreateNewHistory.jsx
+++ src/pages/CreateNewHistory.jsx
@@ -3,7 +3,26 @@ import { useState } from "react";
 export default function CreateNewHistory() {
   return (
     <div>
-      <h1>CreateNewHistory</h1>
+      <form action="">
+        <h1 className="text-3xl text-center mt-6">CreateNewHistory</h1>
+        <div className=" mr-auto ml-auto text-center">
+          <input
+            className=" w-11/12 border p-2 mt-6 "
+            type="text"
+            placeholder="Product Name"
+          />
+          <input
+            className=" w-11/12 border p-2 mt-4 "
+            type="text"
+            placeholder="Quantity"
+          />
+          <input
+            className=" w-11/12 border p-2 mt-8 bg-sky-600 text-white rounded-[0.5rem]"
+            type="submit"
+            value="Submit"
+          />
+        </div>
+      </form>
     </div>
   );
 }
